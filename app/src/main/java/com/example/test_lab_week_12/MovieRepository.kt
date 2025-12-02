9c821397ac352c4876b7417040cdf0545ef21557
BenArief <benarief29@gmail.com>
2025-12-03 00:33:40 +0700

    Commit No.2 - Building app with coroutines

--- app/src/main/java/com/example/test_lab_week_12/MovieRepository.kt
+++ app/src/main/java/com/example/test_lab_week_12/MovieRepository.kt
@@ -1,7 +1,30 @@
 package com.example.test_lab_week_12
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.example.test_lab_week_12.api.MovieService
+import com.example.test_lab_week_12.model.Movie
 
 class MovieRepository(private val movieService: MovieService) {
+    private val apiKey = "f82cd49e71d7b5af3cd1b28c2ad14ce0"
 
+    private val movieLiveData = MutableLiveData<List<Movie>>()
+    val movies: LiveData<List<Movie>>
+        get() = movieLiveData
+
+    private val errorLiveData = MutableLiveData<String>()
+    val error: LiveData<String>
+        get() = errorLiveData
+
+    suspend fun fetchMovies(){
+        try {
+            val popularMovies = movieService.getPopularMovies(apiKey)
+            movieLiveData.postValue(popularMovies.results)
+        } catch (exception : Exception){
+
+            errorLiveData.postValue(
+                "An Error Occured: ${exception.message}"
+            )
+        }
+    }
 }
--- app/src/main/java/com/example/test_lab_week_12/api/MovieService.kt
+++ app/src/main/java/com/example/test_lab_week_12/api/MovieService.kt
@@ -1,4 +1,14 @@
 package com.example.test_lab_week_12.api
 
+import androidx.lifecycle.viewmodel.CreationExtras
+import com.example.test_lab_week_12.model.PopularMoviesResponse
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 interface MovieService {
+    @GET("movie/popular")
+
+    suspend fun getPopularMovies(
+        @Query("api_key")  apiKey: String
+    ): PopularMoviesResponse
 }
